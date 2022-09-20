b45e37520648d7cb32fa8be0af835d33415489f2
trevv16 <tnjeru16@gmail.com>
2022-09-19 20:08:00 -0400

    simplify buttonfunctions down to single math helpers file, and update imports in button.js

--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,12 @@
-import addition from "./buttonFunctions/addition";
-import subtraction from "./buttonFunctions/subtraction";
-import division from "./buttonFunctions/division";
-import multiply from "./buttonFunctions/multiplication";
+import { addition, subtraction, multiply, division } from "../helpers/math";
 
 export default function Button(props) {
-
   //should return a circle with text in it
-  return <div className="Button">
-    <Button onClick={props.function}
-    color={props.color}
-  >
-    {props.text}
-  </Button>
-  </div>;
+  return (
+    <div className="Button">
+      <Button onClick={props.function} color={props.color}>
+        {props.text}
+      </Button>
+    </div>
+  );
 }
--- src/components/buttonFunctions/division.js
+++
@@ -1,3 +0,0 @@
-export default function division(x, y){
-    return x / y;
-};
