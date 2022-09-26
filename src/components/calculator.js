7030b74ccbb2344d4a75004ae70299e0f9d41424
trevv16 <tnjeru16@gmail.com>
2022-09-26 16:55:46 -0400

    added some structure to the calculator component and some todos to be completed

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,19 +1,55 @@
+import { useState } from "react";
+
 import Display from "./display";
 import Button from "./button";
 
 export default function Calculator() {
+  const [displayValue, setDisplayValue] = useState(0);
+
+  const updateDisplayValue = (inputValue) => {
+    let updatedDisplayValue = displayValue;
+
+    switch (inputValue) {
+      case ".":
+        if (displayValue.includes(".")) {
+          // do nothing
+        } else {
+          updatedDisplayValue += inputValue;
+        }
+        break;
+
+        {
+          /* TODO: Add remaining cases */
+        }
+      case "clear":
+        break;
+
+      default:
+        break;
+    }
+
+    setDisplayValue(updatedDisplayValue);
+  };
+
   return (
     <div>
-      {/* props needed: */}
-      {/* - displayValue (string) */}
-      <Display displayValue="10" />
-      <Button color="dark grey" text="5" />
-
-      {/* props needed: */}
-      {/* - displayValue (string) */}
-      {/* - background color (string) */}
-      {/* - onClick (function) */}
-      {/* <Button /> */}
+      <Display displayValue={displayValue} />
+      {/* TODO: Add remaining buttons */}
+      <Button
+        color="dark grey"
+        text="AC"
+        function={() => updateDisplayValue("5")}
+      />
+      <Button
+        color="dark grey"
+        text="5"
+        function={() => updateDisplayValue("4")}
+      />
+      <Button
+        color="dark grey"
+        text="+/−"
+        function={() => updateDisplayValue("")}
+      />
     </div>
   );
 }
